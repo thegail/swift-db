ffd9644c62ba2bd4a1e53229d79a37f18d49a391
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-10-04 09:54:29 -0700

    Add collections field to database

--- src/database/database.rs
+++ src/database/database.rs
@@ -1,15 +1,18 @@
 use crate::archive::BlockFileReader;
+use crate::schema::Schema;
 use std::fs::File;
 use std::io;
 
 pub struct Database {
-    reader: BlockFileReader,
+    pub reader: BlockFileReader,
+    pub collections: Vec<Schema>,
 }
 
 impl Database {
-    fn new(path: String) -> Result<Self, io::Error> {
+    fn new(path: String, collections: Vec<Schema>) -> Result<Self, io::Error> {
         Ok(Self {
             reader: BlockFileReader::new(File::open(path)?),
+            collections,
         })
     }
 }
