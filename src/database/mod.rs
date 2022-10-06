2db9cad8fd235b8a3757528dcb479c0d0a63921c
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-10-05 17:04:46 -0700

    Add find operation

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,3 +1,4 @@
-mod create;
 mod database;
+mod operation_error;
+mod operations;
 mod query;
--- src/database/operations.rs
+++ src/database/operations.rs
@@ -1,6 +1,29 @@
 use super::database::Database;
+use super::operation_error::OperationError;
+use super::query::Query;
+use crate::archive::ArchiveParser;
 use crate::schema::Document;
 
 impl Database {
-    pub fn find(&mut self, collection: u64, query: Query) {}
+    pub fn find(&mut self, collection: u64, query: Query) -> Result<Document, OperationError> {
+        let schema = self
+            .collections
+            .iter()
+            .find(|s| s.id == collection)
+            .ok_or(OperationError::UnknownSchemaIdentifier)?;
+        self.reader
+            .reset_position()
+            .map_err(|e| OperationError::ReadError(e))?;
+        loop {
+            let block = self
+                .reader
+                .next()
+                .map_err(|o| OperationError::ReadError(o))?;
+            let mut parser =
+                ArchiveParser::new(schema.clone(), block, query.fields_of_interest.clone());
+            return parser
+                .read_document()
+                .map_err(|e| OperationError::ParseError(e));
+        }
+    }
 }
