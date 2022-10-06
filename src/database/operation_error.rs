a54e1d2187427511f39ce70d32b82fe233835f63
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-10-05 17:04:33 -0700

    Add operation error type

---
+++ src/database/operation_error.rs
@@ -0,0 +1,17 @@
+use crate::archive::ParseError;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug)]
+pub enum OperationError {
+    ParseError(ParseError),
+    ReadError(std::io::Error),
+    UnknownSchemaIdentifier,
+}
+
+impl Display for OperationError {
+    fn fmt(&self, formatter: &mut Formatter) -> Result<(), std::fmt::Error> {
+        write!(formatter, "{}", self)
+    }
+}
+impl Error for OperationError {}
