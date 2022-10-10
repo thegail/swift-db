00736162df64878597b1b5b80a7fb36c30e9c346
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-10-10 12:34:22 -0700

    Add schema ID to serialized documents

--- src/archive/document_serialize.rs
+++ src/archive/document_serialize.rs
@@ -3,12 +3,14 @@ use crate::schema::{Document, FieldValue};
 impl Document {
     pub fn serialize(&self) -> Vec<u8> {
         let mut vector = Vec::<u8>::new();
+        let schema_bytes = self.schema.id.to_be_bytes();
+        vector.extend_from_slice(&schema_bytes);
         for field in self.fields.iter() {
             let bytes = field.id.to_be_bytes();
             vector.extend_from_slice(&bytes);
             vector.append(&mut field.value.serialize());
         }
-        vec![]
+        vector
     }
 }
 
@@ -28,8 +30,16 @@ impl FieldValue {
                 }
             }
             FieldValue::DateTime(d) => d.timestamp().to_be_bytes().to_vec(),
-            FieldValue::String(s) => s.as_bytes().to_vec(),
-            FieldValue::ByteArray(b) => b.to_vec(),
+            FieldValue::String(s) => {
+                let mut bytes = s.len().to_be_bytes().to_vec();
+                bytes.extend_from_slice(s.as_bytes());
+                bytes
+            }
+            FieldValue::ByteArray(b) => {
+                let mut bytes = b.len().to_be_bytes().to_vec();
+                bytes.extend_from_slice(b);
+                bytes
+            }
             FieldValue::Array(_) => todo!(),
             FieldValue::Object(_) => todo!(),
             FieldValue::Enum(_) => todo!(),
