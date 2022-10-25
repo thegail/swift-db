246c09da696d1e27df5dd6b18bdd39860b6f1f7b
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-10-24 17:12:19 -0700

    Add OperationError::ExpressionTypeMismatch

--- src/database/operation_error.rs
+++ src/database/operation_error.rs
@@ -1,12 +1,20 @@
 use crate::archive::ParseError;
+use crate::schema::FieldType;
 use std::error::Error;
-use std::fmt::{Display, Formatter};
+use std::fmt::{Debug, Display, Formatter};
 
 #[derive(Debug)]
 pub enum OperationError {
     ParseError(ParseError),
     IOError(std::io::Error),
     UnknownSchemaIdentifier,
+    ExpressionTypeMismatch(ExpressionTypeMismatch),
+}
+
+impl OperationError {
+    pub fn expression_type_mismatch(left: FieldType, right: FieldType) -> Self {
+        Self::ExpressionTypeMismatch(ExpressionTypeMismatch { left, right })
+    }
 }
 
 impl Display for OperationError {
@@ -15,3 +23,34 @@ impl Display for OperationError {
     }
 }
 impl Error for OperationError {}
+
+struct ExpressionTypeMismatch {
+    left: FieldType,
+    right: FieldType,
+}
+
+impl Display for FieldType {
+    fn fmt(&self, formatter: &mut Formatter) -> Result<(), std::fmt::Error> {
+        match self {
+            FieldType::Int => write!(formatter, "FieldType::Int"),
+            FieldType::UInt => write!(formatter, "FieldType::UInt"),
+            FieldType::Long => write!(formatter, "FieldType::Long"),
+            FieldType::ULong => write!(formatter, "FieldType::ULong"),
+            FieldType::Float => write!(formatter, "FieldType::Float"),
+            FieldType::Bool => write!(formatter, "FieldType::Bool"),
+            FieldType::DateTime => write!(formatter, "FieldType::DateTime"),
+            FieldType::String => write!(formatter, "FieldType::String"),
+            FieldType::ByteArray => write!(formatter, "FieldType::ByteArray"),
+            FieldType::Array(a) => write!(formatter, "FieldType::Array({a})"),
+            FieldType::Object(o) => write!(formatter, "FieldType::Object({})", o.id),
+            FieldType::Enum(_) => write!(formatter, "FieldType::Enum"),
+        }
+    }
+}
+
+impl Debug for ExpressionTypeMismatch {
+    fn fmt(&self, formatter: &mut Formatter) -> Result<(), std::fmt::Error> {
+        write!(formatter, "left: {}, right: {}", self.left, self.right);
+        Ok(())
+    }
+}
