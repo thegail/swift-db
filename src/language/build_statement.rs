c806013986b5e1feaa87611ab86a0b523c05a450
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-02 15:24:54 -0700

    Refactor expression unwraps to methods

--- src/language/build_statement.rs
+++ src/language/build_statement.rs
@@ -1,7 +1,7 @@
 use super::expression::Expression;
 
 fn build_statement(expression: Vec<Expression>) {
-    let keyword = get_identifier(expression.first().unwrap());
+    let keyword = expression.first().unwrap().get_identifier();
     match keyword.as_str() {
         "select" => build_select(expression),
         "read" => build_read(expression),
@@ -13,38 +13,17 @@ fn build_select(expression: Vec<Expression>) {
     if expression.len() < 6 {
         panic!("error!");
     }
-    let _selection_identifier = get_identifier(&expression[1]);
-    let _transaction_identifier = get_identifier(&expression[2]);
-    let _lock_type = get_identifier(&expression[3]);
-    let collection_expression = get_expression(&expression[4]);
-    if collection_expression.len() < 2 || get_identifier(&collection_expression[0]) != "coll" {
+    let _selection_identifier = expression[1].get_identifier();
+    let _transaction_identifier = expression[2].get_identifier();
+    let _lock_type = expression[3].get_identifier();
+    let collection_expression = expression[4].get_expression();
+    if collection_expression.len() < 2 || collection_expression[0].get_identifier() != "coll" {
         panic!("error!");
     }
-    let _collection_name = get_literal(&collection_expression[1]);
+    let _collection_name = collection_expression[1].get_literal();
     let _condition = 0;
 }
 
 fn build_read(_expression: Vec<Expression>) {
     todo!()
-}
-
-fn get_identifier<'a>(expression: &'a Expression) -> &'a String {
-    match expression {
-        Expression::Identifier(k) => k,
-        _ => panic!("unexpected token"),
-    }
-}
-
-fn get_expression<'a>(expression: &'a Expression) -> &'a Vec<Expression> {
-    match expression {
-        Expression::List(v) => v,
-        _ => panic!("unexpected token"),
-    }
-}
-
-fn get_literal<'a>(expression: &'a Expression) -> &'a String {
-    match expression {
-        Expression::Literal(s) => s,
-        _ => panic!("unexpected token"),
-    }
 }
--- src/language/expression.rs
+++ src/language/expression.rs
@@ -5,3 +5,26 @@ pub enum Expression {
     Literal(String),
     List(Vec<Expression>),
 }
+
+impl Expression {
+    pub fn get_identifier<'a>(&'a self) -> &'a String {
+        match self {
+            Expression::Identifier(k) => k,
+            _ => panic!("unexpected token"),
+        }
+    }
+
+    pub fn get_expression<'a>(&'a self) -> &'a Vec<Expression> {
+        match self {
+            Expression::List(v) => v,
+            _ => panic!("unexpected token"),
+        }
+    }
+
+    pub fn get_literal<'a>(&'a self) -> &'a String {
+        match self {
+            Expression::Literal(s) => s,
+            _ => panic!("unexpected token"),
+        }
+    }
+}
