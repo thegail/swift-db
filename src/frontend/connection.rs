3f701a0e89eabb884b1ddb03437c75360eced452
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-06 15:22:47 -0800

    Move execute statement method to Connection

--- src/frontend/connection.rs
+++ src/frontend/connection.rs
@@ -1,6 +1,37 @@
 use super::transaction::Transaction;
+use crate::language::Statement;
 use std::collections::HashMap;
 
 pub struct Connection {
     pub transactions: HashMap<String, Transaction>,
+}
+
+impl Connection {
+    fn execute_statement(&mut self, statement: Statement) {
+        match statement {
+            Statement::Open { transaction } => {
+                todo!()
+            }
+            Statement::Acquire { transaction } => {
+                todo!()
+            }
+            Statement::Commit { transaction } => {
+                todo!()
+            }
+            Statement::Close { transaction } => {
+                todo!()
+            }
+            Statement::Select {
+                identifier,
+                transaction,
+                query,
+            } => {
+                todo!()
+            }
+            Statement::ReadAll { selection } => {
+                todo!()
+            }
+            _ => todo!(),
+        }
+    }
 }
--- src/frontend/transaction.rs
+++ src/frontend/transaction.rs
@@ -1,37 +1,6 @@
 use crate::backend::Selection;
-use crate::language::Statement;
 use std::collections::HashMap;
 
 pub struct Transaction {
     pub selections: HashMap<String, Selection>,
-}
-
-impl Transaction {
-    fn execute_statement(&mut self, statement: Statement) {
-        match statement {
-            Statement::Open { transaction } => {
-                todo!()
-            }
-            Statement::Acquire { transaction } => {
-                todo!()
-            }
-            Statement::Commit { transaction } => {
-                todo!()
-            }
-            Statement::Close { transaction } => {
-                todo!()
-            }
-            Statement::Select {
-                identifier,
-                transaction,
-                query,
-            } => {
-                todo!()
-            }
-            Statement::ReadAll { selection } => {
-                todo!()
-            }
-            _ => todo!(),
-        }
-    }
 }
