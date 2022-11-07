be7641ea024c8e9d3ff2af8854ea6b8a338aa45a
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-07 14:49:42 -0800

    Move backend implementation inline

--- src/backend/backend.rs
+++ src/backend/backend.rs
@@ -1,4 +1,8 @@
+use super::operation_error::OperationError;
+use super::query::Query;
+use super::selection::{ManySelection, Selection};
 use crate::archive::BlockFileIO;
+use crate::archive::{ArchiveParser, ParseError};
 use crate::schema::{Document, Schema};
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
@@ -6,10 +10,10 @@ use std::io;
 use std::sync::mpsc::Receiver;
 
 pub struct Backend {
-    pub(super) io: BlockFileIO,
-    pub(super) collections: Vec<Schema>,
-    pub(super) document_cache: HashMap<usize, Document>,
-    pub(super) reciever: Receiver<String>,
+    io: BlockFileIO,
+    collections: Vec<Schema>,
+    document_cache: HashMap<usize, Document>,
+    reciever: Receiver<String>,
 }
 
 impl Backend {
@@ -27,5 +31,122 @@ impl Backend {
             document_cache: HashMap::new(),
             reciever,
         })
+    }
+}
+
+impl Backend {
+    pub fn create(&mut self, document: Document) -> Result<(), OperationError> {
+        let bytes = document.serialize();
+        self.io.write_block(bytes).map_err(OperationError::IOError)
+    }
+
+    pub fn find_one(&mut self, query: Query) -> Result<Selection, OperationError> {
+        let schema = self
+            .collections
+            .iter()
+            .find(|s| s.id == query.collection)
+            .ok_or(OperationError::UnknownSchemaIdentifier)?;
+        self.io.reset_position().map_err(OperationError::IOError)?;
+        loop {
+            let (position, block) = self.io.next().map_err(OperationError::IOError)?;
+            let mut parser = ArchiveParser::new(
+                schema.clone(),
+                block,
+                // TODO optimize
+                schema.fields.iter().map(|f| f.id).collect(),
+            );
+            let document_result = parser.read_document();
+            match document_result {
+                Err(ParseError::SchemaMismatch) => {}
+                Err(error) => return Err(OperationError::ParseError(error)),
+                Ok(document) => {
+                    let matches = document.evaluate(&query.condition)?;
+                    if matches {
+                        self.document_cache.insert(position, document);
+                        return Ok(Selection {
+                            position,
+                            schema: schema.clone(),
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn find_many(&mut self, query: Query) -> Result<ManySelection, OperationError> {
+        let schema = self
+            .collections
+            .iter()
+            .find(|s| s.id == query.collection)
+            .ok_or(OperationError::UnknownSchemaIdentifier)?;
+        self.io.reset_position().map_err(OperationError::IOError)?;
+        let mut results = vec![];
+        loop {
+            let next = self.io.next();
+            if let Err(error) = &next {
+                if let std::io::ErrorKind::UnexpectedEof = error.kind() {
+                    break;
+                }
+            }
+            let (position, block) = next.unwrap();
+            let mut parser = ArchiveParser::new(
+                schema.clone(),
+                block,
+                // TODO optimize
+                schema.fields.iter().map(|f| f.id).collect(),
+            );
+            let document_result = parser.read_document();
+            match document_result {
+                Err(ParseError::SchemaMismatch) => {}
+                Err(error) => return Err(OperationError::ParseError(error)),
+                Ok(document) => {
+                    let matches = document.evaluate(&query.condition)?;
+                    self.document_cache.insert(position, document);
+                    if matches {
+                        results.push(position)
+                    }
+                }
+            }
+        }
+        Ok(ManySelection {
+            schema: schema.clone(),
+            positions: results,
+        })
+    }
+
+    pub fn read(
+        &mut self,
+        selection: Selection,
+        fields: Vec<u16>,
+    ) -> Result<Document, OperationError> {
+        let block = self
+            .io
+            .read_at_position(selection.position as u64)
+            .map_err(OperationError::IOError)?;
+        let document = ArchiveParser::new(selection.schema, block, fields)
+            .read_document()
+            .map_err(OperationError::ParseError)?;
+        Ok(document)
+    }
+
+    pub fn read_many(
+        &mut self,
+        selection: ManySelection,
+        fields: Vec<u16>,
+    ) -> Result<Vec<Document>, OperationError> {
+        selection
+            .positions
+            .iter()
+            .map(|p| {
+                let block = self
+                    .io
+                    .read_at_position(*p as u64)
+                    .map_err(OperationError::IOError)?;
+                let document = ArchiveParser::new(selection.schema.clone(), block, fields.clone())
+                    .read_document()
+                    .map_err(OperationError::ParseError)?;
+                Ok(document)
+            })
+            .collect()
     }
 }
--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,6 +1,5 @@
 mod backend;
 mod operation_error;
-mod operations;
 mod query;
 mod selection;
 #[cfg(test)]
