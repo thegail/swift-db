037ac4338a891b543a23c44da3603c4b132f19e8
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-07 15:24:01 -0800

    Add FrontendError::OperationError

--- src/frontend/frontend_error.rs
+++ src/frontend/frontend_error.rs
@@ -1,9 +1,11 @@
+use crate::backend::OperationError;
 use std::error::Error;
 use std::fmt::{Display, Formatter};
 
 #[derive(Debug)]
 pub enum FrontendError {
     Redeclaration { identifier: String },
+    OperationError(OperationError),
 }
 
 impl Display for FrontendError {
@@ -11,6 +13,9 @@ impl Display for FrontendError {
         match self {
             FrontendError::Redeclaration { identifier } => {
                 write!(formatter, "Redeclaration of identifier {}", identifier)
+            }
+            FrontendError::OperationError(error) => {
+                write!(formatter, "Backend operation error: {}", error)
             }
         }
     }
