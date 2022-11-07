ec06c40ca32295866bc657ed8368c1d4fa284f32
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-07 09:34:35 -0800

    Add reciever field on Backend

--- src/backend/backend.rs
+++ src/backend/backend.rs
@@ -3,15 +3,21 @@ use crate::schema::{Document, Schema};
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
 use std::io;
+use std::sync::mpsc::Receiver;
 
 pub struct Backend {
     pub io: BlockFileIO,
     pub collections: Vec<Schema>,
     pub document_cache: HashMap<usize, Document>,
+    pub reciever: Receiver<String>,
 }
 
 impl Backend {
-    pub fn new(path: String, collections: Vec<Schema>) -> Result<Self, io::Error> {
+    pub fn new(
+        path: String,
+        collections: Vec<Schema>,
+        reciever: Receiver<String>,
+    ) -> Result<Self, io::Error> {
         Ok(Self {
             io: BlockFileIO::new(
                 File::open(&path)?,
@@ -19,6 +25,7 @@ impl Backend {
             ),
             collections,
             document_cache: HashMap::new(),
+            reciever,
         })
     }
 }
