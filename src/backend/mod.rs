9ca2ac15289c712defdf15bd94e6fc1e9924a587
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-07 18:08:28 -0800

    Rename module operation to request

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,13 +1,13 @@
 mod backend;
-mod operation;
 mod operation_error;
 mod query;
+mod request;
 mod selection;
 #[cfg(test)]
 mod tests;
 
 pub use backend::Backend;
-pub use operation::{Operation, Request, Response};
 pub use operation_error::OperationError;
 pub use query::{Condition, Expression, Query};
+pub use request::{Operation, Request, Response};
 pub use selection::Selection;
