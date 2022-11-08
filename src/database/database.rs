77ad127450388f9aa315ead85123edc8eceb2b7c
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-07 20:16:02 -0800

    Add database listen logic

--- src/database/database.rs
+++ src/database/database.rs
@@ -2,11 +2,12 @@ use super::lifecycle_error::LifecycleError;
 use crate::backend::{Backend, Request};
 use crate::frontend::Connection;
 use crate::schema::Schema;
+use std::net::TcpListener;
 use std::sync::mpsc::{channel, Sender};
+use std::thread::spawn;
 
 pub struct Database {
     backend: Backend,
-    connections: Vec<Connection>,
     sender: Sender<Request>,
 }
 
@@ -16,9 +17,25 @@ impl Database {
         let db = Self {
             backend: Backend::new(path, collections, reciever)
                 .map_err(LifecycleError::BackendError)?,
-            connections: Vec::new(),
             sender,
         };
         Ok(db)
+    }
+
+    pub fn start(mut self) -> Result<(), LifecycleError> {
+        spawn(move || {
+            self.backend.listen();
+        });
+        let listener = TcpListener::bind("localhost:1952").map_err(LifecycleError::NetworkError)?;
+        for connection in listener.incoming() {
+            match connection {
+                Ok(stream) => {
+                    let mut connection = Connection::new(stream, self.sender.clone());
+                    spawn(move || connection.listen());
+                }
+                Err(_) => (),
+            }
+        }
+        Ok(())
     }
 }
