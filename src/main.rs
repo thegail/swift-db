274ca897c702f8f0fa1368bd80eebad97244420f
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-07 20:17:49 -0800

    Add main logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use database::{Database, LifecycleError};
+use test_stuff::test_schema;
+
 // TODO remove dead code
 // TODO rewrite all imports
 #[allow(dead_code)]
@@ -15,6 +18,94 @@ mod schema;
 #[allow(dead_code)]
 mod util;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), LifecycleError> {
+    let database = Database::new("test.sdb".to_string(), vec![test_schema()])?;
+    database.start()?;
+    Ok(())
+}
+
+mod test_stuff {
+    use crate::schema::{EnumCase, Field, FieldType, Schema};
+    fn test_fields() -> Vec<Field> {
+        vec![
+            Field {
+                name: "name".to_string(),
+                id: 0x1,
+                field_type: FieldType::String,
+            },
+            Field {
+                name: "apple_count".to_string(),
+                id: 0x2,
+                field_type: FieldType::UInt,
+            },
+            Field {
+                name: "money".to_string(),
+                id: 0x3,
+                field_type: FieldType::Float,
+            },
+            Field {
+                name: "has_pet".to_string(),
+                id: 0x4,
+                field_type: FieldType::Bool,
+            },
+            Field {
+                name: "birthday".to_string(),
+                id: 0x5,
+                field_type: FieldType::DateTime,
+            },
+            Field {
+                name: "state".to_string(),
+                id: 0x6,
+                field_type: FieldType::Enum(vec![
+                    EnumCase {
+                        id: 0x1,
+                        name: "happy".to_string(),
+                        associated_value: None,
+                    },
+                    EnumCase {
+                        id: 0x2,
+                        name: "sad".to_string(),
+                        associated_value: None,
+                    },
+                    EnumCase {
+                        id: 0x3,
+                        name: "angry".to_string(),
+                        associated_value: None,
+                    },
+                ]),
+            },
+            Field {
+                name: "coordinates".to_string(),
+                id: 0x7,
+                field_type: FieldType::Object(Box::new(coords_schema())),
+            },
+        ]
+    }
+
+    fn coords_schema() -> Schema {
+        Schema {
+            name: "coordinates".to_string(),
+            id: 0x20,
+            fields: vec![
+                Field {
+                    name: "x".to_string(),
+                    id: 0x1,
+                    field_type: FieldType::Int,
+                },
+                Field {
+                    name: "y".to_string(),
+                    id: 0x2,
+                    field_type: FieldType::Int,
+                },
+            ],
+        }
+    }
+
+    pub fn test_schema() -> Schema {
+        Schema {
+            name: "people".to_string(),
+            id: 0x10,
+            fields: test_fields(),
+        }
+    }
 }
