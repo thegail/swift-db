ea8f91028630c89f8f923fd2efc430c56f3c5e02
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-07 18:45:33 -0800

    Derive clone for Selection

--- src/backend/selection.rs
+++ src/backend/selection.rs
@@ -1,10 +1,12 @@
 use crate::schema::Schema;
 
+#[derive(Clone)]
 pub struct Selection {
     pub schema: Schema,
     pub position: usize,
 }
 
+#[derive(Clone)]
 pub struct ManySelection {
     pub schema: Schema,
     pub positions: Vec<usize>,
