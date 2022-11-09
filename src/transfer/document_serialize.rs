602932dbd31fde2795ecc10189aff296987e976f
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-08 20:39:03 -0800

    Add serialize_list function

--- src/transfer/document_serialize.rs
+++ src/transfer/document_serialize.rs
@@ -2,15 +2,9 @@ use crate::schema::{Document, FieldValue, Schema};
 use std::io::Write;
 
 impl Document {
-    fn serialize(&self, out: &mut impl Write, schema: &Schema) -> Result<(), std::io::Error> {
+    fn h_serialize(&self, out: &mut impl Write, schema: &Schema) -> Result<(), std::io::Error> {
         write!(out, "{{")?;
-        let is_first = true;
-        for field in self.fields {
-            if is_first {
-                is_first = false;
-            } else {
-                write!(out, ",")?;
-            }
+        serialize_list(out, self.fields, |field| {
             let definition = schema
                 .fields
                 .iter()
@@ -18,21 +12,21 @@ impl Document {
                 .ok_or(todo!("field not found"))?;
             write!(out, "\"{}\":", definition.name.escape_default())?;
             field.value.serialize(out);
-        }
+        });
         write!(out, "}}")?;
         Ok(())
     }
 }
 
 impl FieldValue {
-    fn serialize(&self, out: &mut impl Write) -> Result<(), std::io::Error> {
+    fn h_serialize(&self, out: &mut impl Write) -> Result<(), std::io::Error> {
         match self {
             FieldValue::Int(i) => write!(out, "{}", i),
             FieldValue::UInt(i) => write!(out, "{}", i),
             FieldValue::Long(i) => write!(out, "{}", i),
             FieldValue::ULong(i) => write!(out, "{}", i),
             FieldValue::Float(f) => write!(out, "{}", f),
-            FieldValue::Bool(b) => write!(out, "{}", if b { "true" } else { "false" }),
+            FieldValue::Bool(b) => write!(out, "{}", if *b { "true" } else { "false" }),
             FieldValue::DateTime(d) => write!(out, "{}", d.timestamp()),
             FieldValue::String(s) => write!(out, "\"{}\"", s.escape_default()),
             FieldValue::ByteArray(b) => {
@@ -54,4 +48,25 @@ impl FieldValue {
             FieldValue::Enum(_) => todo!(),
         }
     }
+}
+
+fn serialize_list<I, F, W>(
+    out: &mut W,
+    values: impl Iterator<Item = I>,
+    callback: F,
+) -> Result<(), std::io::Error>
+where
+    W: Write,
+    F: Fn(I, &mut W) -> Result<(), std::io::Error>,
+{
+    let is_first = true;
+    for value in values {
+        if is_first {
+            is_first = false;
+        } else {
+            write!(out, ",")?;
+        }
+        callback(value, out)?;
+    }
+    Ok(())
 }
