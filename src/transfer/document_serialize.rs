b1dddb6b5a498f2fd8ee6d0eab76a741d5ae7966
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-08 21:39:21 -0800

    Add array and enum serialization logic

--- src/transfer/document_serialize.rs
+++ src/transfer/document_serialize.rs
@@ -1,17 +1,19 @@
-use crate::schema::{Document, FieldValue, Schema};
+use crate::schema::{Document, Field, FieldType, FieldValue};
 use std::io::Write;
 
 impl Document {
-    fn h_serialize(&self, out: &mut impl Write, schema: &Schema) -> Result<(), std::io::Error> {
+    fn h_serialize(&self, out: &mut impl Write) -> Result<(), std::io::Error> {
         write!(out, "{{")?;
-        serialize_list(out, self.fields, |field| {
-            let definition = schema
+        serialize_list(out, self.fields.iter(), |field, out| {
+            let definition = self
+                .schema
                 .fields
                 .iter()
                 .find(|f| f.id == field.id)
-                .ok_or(todo!("field not found"))?;
+                .ok_or_else(|| todo!("field not found") as std::io::Error)?;
             write!(out, "\"{}\":", definition.name.escape_default())?;
-            field.value.serialize(out);
+            field.value.h_serialize(out, definition);
+            Ok(())
         });
         write!(out, "}}")?;
         Ok(())
@@ -19,7 +21,7 @@ impl Document {
 }
 
 impl FieldValue {
-    fn h_serialize(&self, out: &mut impl Write) -> Result<(), std::io::Error> {
+    fn h_serialize(&self, out: &mut impl Write, definition: &Field) -> Result<(), std::io::Error> {
         match self {
             FieldValue::Int(i) => write!(out, "{}", i),
             FieldValue::UInt(i) => write!(out, "{}", i),
@@ -31,21 +33,36 @@ impl FieldValue {
             FieldValue::String(s) => write!(out, "\"{}\"", s.escape_default()),
             FieldValue::ByteArray(b) => {
                 write!(out, "[")?;
-                let is_first = true;
-                for byte in b {
-                    if is_first {
-                        is_first = false;
-                    } else {
-                        write!(out, ",")?;
-                    }
-                    write!(out, "{}", byte)?;
-                }
+                serialize_list(out, b.iter(), |byte, out| write!(out, "{}", byte))?;
                 write!(out, "]")?;
                 Ok(())
             }
-            FieldValue::Array(_) => todo!(),
-            FieldValue::Object(_) => todo!(),
-            FieldValue::Enum(_) => todo!(),
+            FieldValue::Array(a) => {
+                write!(out, "[")?;
+                serialize_list(out, a.iter(), |value, out| {
+                    value.h_serialize(out, definition)
+                })?;
+                write!(out, "]")?;
+                Ok(())
+            }
+            FieldValue::Object(o) => o.h_serialize(out),
+            FieldValue::Enum(e) => {
+                let cases = match &definition.field_type {
+                    FieldType::Enum(cases) => cases,
+                    _ => todo!("err handling"),
+                };
+                let case = cases
+                    .iter()
+                    .find(|c| c.id == e.case_id)
+                    .ok_or_else(|| todo!("err handling") as std::io::Error)?;
+                write!(out, "{{\"{}\":{{", case.name)?;
+                if let Some(value) = &e.associated_value {
+                    write!(out, "_0:")?;
+                    value.h_serialize(out, definition)?;
+                }
+                write!(out, "}}")?;
+                Ok(())
+            }
         }
     }
 }
@@ -59,7 +76,7 @@ where
     W: Write,
     F: Fn(I, &mut W) -> Result<(), std::io::Error>,
 {
-    let is_first = true;
+    let mut is_first = true;
     for value in values {
         if is_first {
             is_first = false;
