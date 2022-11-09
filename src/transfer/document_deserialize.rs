3380c9f9d0b815a10c3de6ad37af9a938b6b6b95
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-09 09:52:11 -0800

    Add document visitor implementation

--- src/transfer/document_deserialize.rs
+++ src/transfer/document_deserialize.rs
@@ -1,3 +1,5 @@
+use crate::schema::Document;
+use serde::de::Visitor;
 use serde::Deserialize;
 
 struct BareDocument {
@@ -24,11 +26,36 @@ struct BareEnumValue {
     associated_value: Option<BareValue>,
 }
 
+struct DocumentVisitor;
+
+impl<'de> Visitor<'de> for DocumentVisitor {
+    type Value = BareDocument;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        write!(formatter, "a map of key-value pairs")
+    }
+
+    fn visit_map<A>(self, map: A) -> Result<Self::Value, A::Error>
+    where
+        A: serde::de::MapAccess<'de>,
+    {
+        let mut fields = Vec::with_capacity(map.size_hint().unwrap_or(0));
+        while let Some((key, value)) = map.next_entry()? {
+            fields.push(BareField {
+                name: key,
+                value: todo!(),
+            });
+        }
+        let document = BareDocument { fields };
+        Ok(document)
+    }
+}
+
 impl<'de> Deserialize<'de> for BareDocument {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: serde::Deserializer<'de>,
     {
-        deserializer.deserialize_bytes(visitor)
+        deserializer.deserialize_map(DocumentVisitor)
     }
 }
