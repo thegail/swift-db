cfb36e943ee614480c162ebaa2bbda4e2eedf0d4
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-09 07:56:23 -0800

    Add bare deserialization types

--- src/transfer/document_deserialize.rs
+++ src/transfer/document_deserialize.rs
@@ -1,10 +1,34 @@
-use crate::schema::Document;
 use serde::Deserialize;
 
-impl<'de> Deserialize<'de> for Document {
+struct BareDocument {
+    fields: Vec<BareField>,
+}
+
+struct BareField {
+    name: String,
+    value: BareValue,
+}
+
+enum BareValue {
+    Integer(i64),
+    Float(f64),
+    Bool(bool),
+    String(String),
+    Array(Vec<BareValue>),
+    Object(Box<BareDocument>),
+    Enum(Box<BareEnumValue>),
+}
+
+struct BareEnumValue {
+    case: String,
+    associated_value: Option<BareValue>,
+}
+
+impl<'de> Deserialize<'de> for BareDocument {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: serde::Deserializer<'de>,
     {
+        deserializer.deserialize_bytes(visitor)
     }
 }
