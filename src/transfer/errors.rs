aacd3022b61d10e81b70b84612c1062e5b8c9135
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-09 14:13:18 -0800

    Add field type mismatch error

--- src/transfer/errors.rs
+++ src/transfer/errors.rs
@@ -3,13 +3,17 @@ use std::fmt::{Display, Formatter};
 #[derive(Debug)]
 pub enum DeserializationError {
     FieldNotFound(String),
-    Overflow(u64),
+    FieldTypeMismatch(String),
+    Overflow(i64),
 }
 
 impl Display for DeserializationError {
     fn fmt(&self, formatter: &mut Formatter) -> Result<(), std::fmt::Error> {
         match self {
             DeserializationError::FieldNotFound(s) => write!(formatter, "Field {} not found", s),
+            DeserializationError::FieldTypeMismatch(f) => {
+                write!(formatter, "Incorrect type in field {}", f)
+            }
             DeserializationError::Overflow(v) => write!(formatter, "Value {} overflows field", v),
         }
     }
