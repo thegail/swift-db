518f8d8c7858691957ad166c846637f0b7b32f64
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-09 14:08:56 -0800

    Add deserialization overflow error

--- src/transfer/errors.rs
+++ src/transfer/errors.rs
@@ -3,12 +3,14 @@ use std::fmt::{Display, Formatter};
 #[derive(Debug)]
 pub enum DeserializationError {
     FieldNotFound(String),
+    Overflow(u64),
 }
 
 impl Display for DeserializationError {
     fn fmt(&self, formatter: &mut Formatter) -> Result<(), std::fmt::Error> {
         match self {
             DeserializationError::FieldNotFound(s) => write!(formatter, "Field {} not found", s),
+            DeserializationError::Overflow(v) => write!(formatter, "Value {} overflows field", v),
         }
     }
 }
