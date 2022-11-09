1b2adcefce589857e9bc284dd517b49147172248
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-08 21:39:58 -0800

    Rename transfer serialization functions

--- src/transfer/document_serialize.rs
+++ src/transfer/document_serialize.rs
@@ -2,7 +2,7 @@ use crate::schema::{Document, Field, FieldType, FieldValue};
 use std::io::Write;
 
 impl Document {
-    fn h_serialize(&self, out: &mut impl Write) -> Result<(), std::io::Error> {
+    fn transfer_serialize(&self, out: &mut impl Write) -> Result<(), std::io::Error> {
         write!(out, "{{")?;
         serialize_list(out, self.fields.iter(), |field, out| {
             let definition = self
@@ -12,7 +12,7 @@ impl Document {
                 .find(|f| f.id == field.id)
                 .ok_or_else(|| todo!("field not found") as std::io::Error)?;
             write!(out, "\"{}\":", definition.name.escape_default())?;
-            field.value.h_serialize(out, definition);
+            field.value.transfer_serialize(out, definition);
             Ok(())
         });
         write!(out, "}}")?;
@@ -21,7 +21,11 @@ impl Document {
 }
 
 impl FieldValue {
-    fn h_serialize(&self, out: &mut impl Write, definition: &Field) -> Result<(), std::io::Error> {
+    fn transfer_serialize(
+        &self,
+        out: &mut impl Write,
+        definition: &Field,
+    ) -> Result<(), std::io::Error> {
         match self {
             FieldValue::Int(i) => write!(out, "{}", i),
             FieldValue::UInt(i) => write!(out, "{}", i),
@@ -40,12 +44,12 @@ impl FieldValue {
             FieldValue::Array(a) => {
                 write!(out, "[")?;
                 serialize_list(out, a.iter(), |value, out| {
-                    value.h_serialize(out, definition)
+                    value.transfer_serialize(out, definition)
                 })?;
                 write!(out, "]")?;
                 Ok(())
             }
-            FieldValue::Object(o) => o.h_serialize(out),
+            FieldValue::Object(o) => o.transfer_serialize(out),
             FieldValue::Enum(e) => {
                 let cases = match &definition.field_type {
                     FieldType::Enum(cases) => cases,
@@ -58,7 +62,7 @@ impl FieldValue {
                 write!(out, "{{\"{}\":{{", case.name)?;
                 if let Some(value) = &e.associated_value {
                     write!(out, "_0:")?;
-                    value.h_serialize(out, definition)?;
+                    value.transfer_serialize(out, definition)?;
                 }
                 write!(out, "}}")?;
                 Ok(())
