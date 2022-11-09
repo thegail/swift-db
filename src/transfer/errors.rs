a8a2fd9b27b0f2fa248f88678b91510e3ae35a30
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-09 14:59:15 -0800

    Add case not found error

--- src/transfer/errors.rs
+++ src/transfer/errors.rs
@@ -3,6 +3,7 @@ use std::fmt::{Display, Formatter};
 #[derive(Debug)]
 pub enum DeserializationError {
     FieldNotFound(String),
+    CaseNotFound(String),
     FieldTypeMismatch,
     Overflow(i64),
 }
@@ -11,6 +12,7 @@ impl Display for DeserializationError {
     fn fmt(&self, formatter: &mut Formatter) -> Result<(), std::fmt::Error> {
         match self {
             DeserializationError::FieldNotFound(s) => write!(formatter, "Field {} not found", s),
+            DeserializationError::CaseNotFound(s) => write!(formatter, "Case {} not found", s),
             DeserializationError::FieldTypeMismatch => write!(formatter, "Incorrect type in field"),
             DeserializationError::Overflow(v) => write!(formatter, "Value {} overflows field", v),
         }
