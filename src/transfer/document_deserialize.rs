cb85fea85735afb833227c78573555b10f161bb3
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-08 23:05:16 -0800

    Add Document deserialization method

---
+++ src/transfer/document_deserialize.rs
@@ -0,0 +1,10 @@
+use crate::schema::Document;
+use serde::Deserialize;
+
+impl<'de> Deserialize<'de> for Document {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+    }
+}
--- src/transfer/mod.rs
+++ src/transfer/mod.rs
@@ -1,1 +1,2 @@
+mod document_deserialize;
 mod document_serialize;
