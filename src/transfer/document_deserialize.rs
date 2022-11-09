96901c967329d15fb1db93b0275624debca72a78
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-09 12:28:11 -0800

    Finish bare document deserialization

--- src/transfer/document_deserialize.rs
+++ src/transfer/document_deserialize.rs
@@ -17,12 +17,6 @@ enum BareValue {
     String(String),
     Array(Vec<BareValue>),
     Object(Box<BareDocument>),
-    Enum(Box<BareEnumValue>),
-}
-
-struct BareEnumValue {
-    case: String,
-    associated_value: Option<BareValue>,
 }
 
 struct DocumentVisitor;
@@ -53,5 +47,129 @@ impl<'de> Deserialize<'de> for BareDocument {
         D: serde::Deserializer<'de>,
     {
         deserializer.deserialize_map(DocumentVisitor)
+    }
+}
+
+struct ValueVisitor;
+
+impl<'de> Visitor<'de> for ValueVisitor {
+    type Value = BareValue;
+
+    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(formatter, "any value")
+    }
+
+    fn visit_i8<E>(self, v: i8) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(BareValue::Integer(v as i64))
+    }
+
+    fn visit_u8<E>(self, v: u8) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(BareValue::Integer(v as i64))
+    }
+
+    fn visit_i16<E>(self, v: i16) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(BareValue::Integer(v as i64))
+    }
+
+    fn visit_u16<E>(self, v: u16) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(BareValue::Integer(v as i64))
+    }
+
+    fn visit_i32<E>(self, v: i32) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(BareValue::Integer(v as i64))
+    }
+
+    fn visit_u32<E>(self, v: u32) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(BareValue::Integer(v as i64))
+    }
+
+    fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(BareValue::Integer(v))
+    }
+
+    fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        if v > i64::MAX as u64 {
+            return Err(serde::de::Error::custom("u64 out of range"));
+        }
+        Ok(BareValue::Integer(v as i64))
+    }
+
+    fn visit_f32<E>(self, v: f32) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(BareValue::Float(v as f64))
+    }
+
+    fn visit_f64<E>(self, v: f64) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(BareValue::Float(v))
+    }
+
+    fn visit_bool<E>(self, v: bool) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(BareValue::Bool(v))
+    }
+
+    fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        Ok(BareValue::String(v))
+    }
+
+    fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+    where
+        A: serde::de::SeqAccess<'de>,
+    {
+        let mut values = Vec::with_capacity(seq.size_hint().unwrap_or(0));
+        while let Some(value) = seq.next_element()? {
+            values.push(value);
+        }
+        Ok(BareValue::Array(values))
+    }
+
+    fn visit_map<A>(self, map: A) -> Result<Self::Value, A::Error>
+    where
+        A: serde::de::MapAccess<'de>,
+    {
+        Ok(BareValue::Object(Box::new(DocumentVisitor.visit_map(map)?)))
+    }
+}
+
+impl<'de> Deserialize<'de> for BareValue {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        deserializer.deserialize_any(ValueVisitor)
     }
 }
