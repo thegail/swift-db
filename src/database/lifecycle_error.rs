40cc2e0e3413a1ae75c6dcc0ba654ce3ec55c706
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-10 09:43:03 -0800

    Add lifecycle error docs

--- src/database/lifecycle_error.rs
+++ src/database/lifecycle_error.rs
@@ -1,6 +1,10 @@
 use std::error::Error;
 use std::fmt::{Display, Formatter};
 
+/// An error during the startup of a [`Database`].
+///
+/// A lifecycle error cannot be recovered from and
+/// should case the process to exit.
 #[derive(Debug)]
 pub enum LifecycleError {
     BackendError(std::io::Error),
