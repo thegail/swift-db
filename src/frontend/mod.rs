1b2844c2245aa3fdbea343ae8717f69e55c024e9
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-10 09:58:08 -0800

    Add frontend module docs

--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -1,3 +1,11 @@
+//! The `frontend` is responsible for managing client connections.
+//!
+//! A [`Connection`] runs on its own thread, invokes the
+//! [`language`][crate::language] parser to build statements, then
+//! executes them. The [`Transaction`][transaction::Transaction] helper
+//! struct manages transaction state and helps in the execution of
+//! statements. Requests are passed to the backend via an MPSC channel,
+//! the response is serialized, and sent back over the network stream.
 mod connection;
 mod frontend_error;
 mod transaction;
