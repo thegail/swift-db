a7d5d683fc71ae729b0d14133bd55976746eedc1
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-10 09:50:09 -0800

    Add database module docs

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,3 +1,12 @@
+//! The `database` module contains the main coordinating logic
+//! of SwiftDB.
+//!
+//! A [`Database`] instance runs on the main thread and is
+//! responsible for startup and configuration, as well as
+//! listening for incoming connections. It starts the
+//! [`Backend`][crate::backend::Backend] thread, and various
+//! [`Connection`][crate::frontend::Connection] threads for
+//! each incoming network connection.
 #[allow(clippy::module_inception)]
 mod database;
 mod lifecycle_error;
