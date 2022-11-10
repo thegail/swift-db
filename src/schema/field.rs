26a2da7a5b5e41de64a56fd354803c8a08405687
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-10 15:08:39 -0800

    Add Field docs

--- src/schema/field.rs
+++ src/schema/field.rs
@@ -1,8 +1,13 @@
 use crate::schema::FieldType;
 
+/// A definition of a field which a
+/// [`Document`][crate::schema::Document] may have.
 #[derive(Clone)]
 pub struct Field {
+    /// The name of this field.
     pub name: String,
+    /// The ID of this field.
     pub id: u16,
+    /// The type of this field.
     pub field_type: FieldType,
 }
