590d140f5fddcc2b586a1a3c8460b644d94732fb
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-10 11:17:40 -0800

    Add Transaction docs

--- src/frontend/transaction.rs
+++ src/frontend/transaction.rs
@@ -1,11 +1,17 @@
 use crate::backend::Selection;
 use std::collections::HashMap;
 
+/// A helper struct for managing transaction state.
+///
+/// Instantiated by a [`Connection`][crate::frontend::Connection]
+/// whenever a new transaction is opened. Helper methods are
+/// used when running queries on this transaction.
 pub struct Transaction {
     pub selections: HashMap<String, Selection>,
 }
 
 impl Transaction {
+    /// Creates a new transaction.
     pub fn new() -> Self {
         Transaction {
             selections: HashMap::new(),
