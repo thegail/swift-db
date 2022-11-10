1609faa05c567acf2222a83b3a0f9d7cb746e55f
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-10 15:04:28 -0800

    Add FieldType docs

--- src/schema/field_type.rs
+++ src/schema/field_type.rs
@@ -1,5 +1,6 @@
 use crate::schema::Schema;
 
+/// The type of a [`Field`][crate::schema::Field].
 #[derive(Clone)]
 pub enum FieldType {
     Int,
@@ -16,9 +17,14 @@ pub enum FieldType {
     Enum(Vec<EnumCase>),
 }
 
+/// A possible value for an enumerated field.
 #[derive(Clone)]
 pub struct EnumCase {
+    /// The name of this enum case.
     pub name: String,
+    /// The ID of this enum case.
     pub id: u16,
+    /// The associated type that is stored along
+    /// with instances this caase.
     pub associated_value: Option<FieldType>,
 }
