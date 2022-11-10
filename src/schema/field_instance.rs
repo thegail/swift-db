c9a4c982f0f0be50a78ce8518aff76d5bf7c01db
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-10 15:02:19 -0800

    Add FieldInstance docs

--- src/schema/field_instance.rs
+++ src/schema/field_instance.rs
@@ -1,7 +1,11 @@
 use crate::schema::FieldValue;
 
+/// A key/value pair for a field.
 #[derive(Clone)]
 pub struct FieldInstance {
+    /// The ID of the field definition that this field
+    /// conforms to.
     pub id: u16,
+    /// The value stored in this field instance.
     pub value: FieldValue,
 }
