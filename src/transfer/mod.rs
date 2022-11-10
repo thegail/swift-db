8b850c7721ff8578217e28bdc78f4b596af7f6ca
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-10 12:16:34 -0800

    Add transfer module docs

--- src/transfer/mod.rs
+++ src/transfer/mod.rs
@@ -1,3 +1,9 @@
+//! The `transfer` module implements JSON serialization for
+//! [`Document`][crate::schema::Document]s.
+//!
+//! Uses [`BareDocument`][bare_document::BareDocument] as
+//! an intermediary format, which implements [`serde::Serialize`]
+//! and [`serde::Deserialize`].
 mod bare_document;
 mod document_deserialize;
 mod document_serialize;
