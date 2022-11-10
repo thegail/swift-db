9ce5fa5e68872be9fd8a9d96045e9866c994a792
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-10 15:18:46 -0800

    Add schema docs

--- src/schema/schema.rs
+++ src/schema/schema.rs
@@ -1,8 +1,12 @@
 use crate::schema::Field;
 
+/// A schema definition.
 #[derive(Clone)]
 pub struct Schema {
+    /// The name of the collection this schema defines.
     pub name: String,
+    /// The ID of the collection this schema defines.
     pub id: u64,
+    /// The field definitions on this schema.
     pub fields: Vec<Field>,
 }
