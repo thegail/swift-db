11ee762bc4b932be27bc2fb6409bfb8247b00fa0
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-10 12:20:24 -0800

    Add bare_document docs

--- src/transfer/bare_document.rs
+++ src/transfer/bare_document.rs
@@ -1,12 +1,19 @@
+/// An intermediate format for serializing and
+/// deserializing [`Document`][crate::schema::Document]s.
+///
+/// Implements [`serde::Serialize`] and
+/// [`serde::Deserialize`].
 pub struct BareDocument {
     pub fields: Vec<BareField>,
 }
 
+/// A member of a [`BareDocument`].
 pub struct BareField {
     pub name: String,
     pub value: BareValue,
 }
 
+/// A value in a [`BareField`].
 pub enum BareValue {
     Integer(i64),
     Float(f64),
