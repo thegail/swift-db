ab1f82033d2a6de7f2f79587a35df2e245dfee40
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-09 21:19:15 -0800

    Add document to bare logic

--- src/transfer/document_serialize.rs
+++ src/transfer/document_serialize.rs
@@ -1,4 +1,5 @@
 use super::bare_document::{BareDocument, BareField, BareValue};
+use super::DeserializationError;
 use crate::schema::{Document, FieldType, FieldValue};
 use serde::ser::{SerializeMap, SerializeSeq, SerializeStruct};
 use serde::{Serialize, Serializer};
@@ -34,6 +35,55 @@ impl Serialize for BareValue {
                 list.end()
             }
             BareValue::Object(o) => o.serialize(serializer),
+        }
+    }
+}
+
+impl Document {
+    fn to_bare(self) -> Result<BareDocument, DeserializationError> {
+        let fields: Result<Vec<BareField>, DeserializationError> = self
+            .fields
+            .into_iter()
+            .map(|field| {
+                let field_name = self
+                    .schema
+                    .fields
+                    .iter()
+                    .find(|f| f.id == field.id)
+                    .ok_or_else(|| DeserializationError::FieldNotFound(field.id.to_string()))?;
+            })
+            .collect();
+        Ok(BareDocument { fields: fields? })
+    }
+}
+
+impl FieldValue {
+    fn to_bare(self) -> Result<BareValue, DeserializationError> {
+        match self {
+            FieldValue::Int(i) => Ok(BareValue::Integer(i as i64)),
+            FieldValue::UInt(i) => Ok(BareValue::Integer(i as i64)),
+            FieldValue::Long(i) => Ok(BareValue::Integer(i as i64)),
+            FieldValue::ULong(i) => {
+                if i > i64::MAX as u64 {
+                    Err(DeserializationError::Overflow(0))
+                } else {
+                    Ok(BareValue::Integer(i as i64))
+                }
+            }
+            FieldValue::Float(f) => Ok(BareValue::Float(f)),
+            FieldValue::Bool(b) => Ok(BareValue::Bool(b)),
+            FieldValue::DateTime(d) => Ok(BareValue::Integer(d.timestamp())),
+            FieldValue::String(s) => Ok(BareValue::String(s)),
+            FieldValue::ByteArray(b) => Ok(BareValue::Array(
+                b.iter().map(|b| BareValue::Integer(*b as i64)).collect(),
+            )),
+            FieldValue::Array(a) => {
+                let values: Result<Vec<BareValue>, DeserializationError> =
+                    a.iter().map(|v| v.to_bare()).collect();
+                Ok(BareValue::Array(values?))
+            }
+            FieldValue::Object(o) => Ok(BareValue::Object(Box::new(o.to_bare()?))),
+            FieldValue::Enum(e) => todo!(),
         }
     }
 }
