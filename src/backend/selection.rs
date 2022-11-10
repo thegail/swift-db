48d47de2214613b2b90528c57abbc55a3ce4483e
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-10 14:41:12 -0800

    Add Selection docs

--- src/backend/selection.rs
+++ src/backend/selection.rs
@@ -1,13 +1,26 @@
 use crate::schema::Schema;
 
+/// A pointer to a [`Document`][crate::schema::Document]
+/// in the storage file.
+///
+/// Can be used in later [`Request`][crate::backend::Request]s
+/// to the [`Backend`][crate::backend::Backend].
 #[derive(Clone)]
 pub struct Selection {
+    /// The [`Schema`] describing the
+    /// [`Document`][crate::schema::Document] pointed to by
+    /// this selection.
     pub schema: Schema,
-    pub position: usize,
+    /// The offset in the storage file at which the
+    /// [`Document`][crate::schema::Document] is stored.
+    pub(super) position: usize,
 }
 
-#[derive(Clone)]
-pub struct ManySelection {
-    pub schema: Schema,
-    pub positions: Vec<usize>,
-}
+// A pointer to a list of [`Document`][crate::schema::Document]s.
+//
+// See [`Selection`].
+// #[derive(Clone)]
+// pub struct ManySelection {
+//     pub schema: Schema,
+//     pub(super) positions: Vec<usize>,
+// }
