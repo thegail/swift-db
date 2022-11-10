d9ce88727012fbcf17f120559da23fbc4ab6d926
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-11-10 15:00:13 -0800

    Add Document docs

--- src/schema/document.rs
+++ src/schema/document.rs
@@ -1,7 +1,10 @@
 use crate::schema::{FieldInstance, Schema};
 
+/// A single record in the database.
 #[derive(Clone)]
 pub struct Document {
+    /// The schema to which this `Document` conforms.
     pub schema: Schema,
+    /// The fields/value pairs on this instance.
     pub fields: Vec<FieldInstance>,
 }
